1d59e3a51b3d0602594c110afffdf9be8b1f8f23
amrendra singh rathore <31726307+daredevil786@users.noreply.github.com>
2019-02-01 15:41:11 +0530

    Add files via upload

---
+++ even.java
@@ -0,0 +1,14 @@
+class even
+{
+public static void main(String args[])
+{
+int X=12;
+for (X=0;X<100;X++)
+{
+if (X % 2==0)
+System.out.println(X+"the number is even ");
+else
+system.out.println(X+"the number is odd");
+}
+}
+}
