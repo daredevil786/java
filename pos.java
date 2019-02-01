fd443f1fc7c95d07a3d3914c909c0bee3ae224ea
amrendra singh rathore <31726307+daredevil786@users.noreply.github.com>
2019-02-01 15:55:43 +0530

    Add files via upload

---
+++ pos.java
@@ -0,0 +1,12 @@
+class pos
+{
+public static void main (String args[])
+{
+int X=-6;
+if (X > 0)
+System.out.println(X+"the number is positive ");
+else
+System.out.println(X+"the number is negative");
+}
+}
+
